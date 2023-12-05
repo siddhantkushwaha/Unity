06a1df5ef0332800589ecbff5d4d938c5f4aac54
siddhantkushwaha <k16.siddhant@gmail.com>
2023-12-05 09:52:42 +0530

    Upgrading things

--- lib/models.g.dart
+++ lib/models.g.dart
@@ -6,36 +6,42 @@ part of 'models.dart';
 // RealmObjectGenerator
 // **************************************************************************
 
-class ClipboardItem extends _ClipboardItem with RealmEntity, RealmObject {
+// ignore_for_file: type=lint
+class ClipboardItem extends _ClipboardItem
+    with RealmEntity, RealmObjectBase, RealmObject {
   ClipboardItem(
     String text,
     int timestamp,
   ) {
-    RealmObject.set(this, 'text', text);
-    RealmObject.set(this, 'timestamp', timestamp);
+    RealmObjectBase.set(this, 'text', text);
+    RealmObjectBase.set(this, 'timestamp', timestamp);
   }
 
   ClipboardItem._();
 
   @override
-  String get text => RealmObject.get<String>(this, 'text') as String;
+  String get text => RealmObjectBase.get<String>(this, 'text') as String;
   @override
-  set text(String value) => RealmObject.set(this, 'text', value);
+  set text(String value) => RealmObjectBase.set(this, 'text', value);
 
   @override
-  int get timestamp => RealmObject.get<int>(this, 'timestamp') as int;
+  int get timestamp => RealmObjectBase.get<int>(this, 'timestamp') as int;
   @override
-  set timestamp(int value) => RealmObject.set(this, 'timestamp', value);
+  set timestamp(int value) => RealmObjectBase.set(this, 'timestamp', value);
 
   @override
   Stream<RealmObjectChanges<ClipboardItem>> get changes =>
-      RealmObject.getChanges<ClipboardItem>(this);
+      RealmObjectBase.getChanges<ClipboardItem>(this);
+
+  @override
+  ClipboardItem freeze() => RealmObjectBase.freezeObject<ClipboardItem>(this);
 
   static SchemaObject get schema => _schema ??= _initSchema();
   static SchemaObject? _schema;
   static SchemaObject _initSchema() {
-    RealmObject.registerFactory(ClipboardItem._);
-    return const SchemaObject(ClipboardItem, [
+    RealmObjectBase.registerFactory(ClipboardItem._);
+    return const SchemaObject(
+        ObjectType.realmObject, ClipboardItem, 'ClipboardItem', [
       SchemaProperty('text', RealmPropertyType.string),
       SchemaProperty('timestamp', RealmPropertyType.int),
     ]);
--- lib/realmUtils.dart
+++ lib/realmUtils.dart
@@ -2,6 +2,7 @@ import 'package:realm/realm.dart';
 import 'models.dart';
 
 Realm getRealm() {
-  var config = Configuration([ClipboardItem.schema]);
-  return Realm(config);
+  var config = Configuration.local([ClipboardItem.schema]);
+  var realm = Realm(config);
+  return realm;
 }
